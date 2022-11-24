eea2f3a7e3ed3ea14a009023f39f0f8a04c885f3
Brian Wyatt <brianlwyatt@gmail.com>
2022-11-23 21:51:18 -0400

    entries save in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var nineAM = document.getElementById('#09');
-var tenAM = document.getElementById('#10');
-var elevenAM = document.getElementById('#11');
-var twelvePM = document.getElementById('#12');
-var onePM = document.getElementById('#13');
-var twoPM = document.getElementById('#14');
-var threePM = document.getElementById('#15');
-var fourPM = document.getElementById('#16');
-var fivePM = document.getElementById('#17');
-var btn = document.getElementById('.btn');
+var nineAM = document.getElementById('09');
+var tenAM = document.getElementById('10');
+var elevenAM = document.getElementById('11');
+var twelvePM = document.getElementById('12');
+var onePM = document.getElementById('13');
+var twoPM = document.getElementById('14');
+var threePM = document.getElementById('15');
+var fourPM = document.getElementById('16');
+var fivePM = document.getElementById('17');
+var btn = document.getElementsByClassName('btn');
 var timeBlocks = document.querySelectorAll('.time-block');
 
 var today = dayjs();
+var tasks = [];
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -22,6 +23,38 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  function displayTasks() {
+    for (var i = 0; i < tasks.length; i++) {
+      task = tasks[i];
+      console.log(task);
+      // if ($(task))
+    }
+  }
+  
+  function loadTasks() {
+    var storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (storedTasks !== null) {
+      tasks = storedTasks;
+    }
+    displayTasks();
+  }
+
+  function storeTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+  $(btn).click(function(event) {
+  event.preventDefault();
+  var tasksInput = this.previousElementSibling.value;
+  if (tasksInput == '') {
+    return;
+  }
+  tasks.push(tasksInput);
+  console.log(tasks);
+  storeTasks();
+  displayTasks();
+});
 
   
 
@@ -38,16 +71,13 @@ $(function () {
     };
   }
   blockClass();
+  loadTasks();
   
   
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
-
-  // TODO: Add code to display the current date in the header of the page.
-  
   
   // Jquery call to the currentDay ID to display the dayjs var function in specified format
   $('#currentDay').text(today.format('MMMM D, YYYY HH:mm A'));
